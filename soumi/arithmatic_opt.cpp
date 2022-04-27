40edb032a01fc3f85cb61c62758adbe8fe296061
Soumi-Pandit <94689716+Soumi-Pandit@users.noreply.github.com>
2022-04-27 17:58:56 +0530

    Create arithmatic_opt.cpp
    types of arithmetic  operators

---
+++ soumi/arithmatic_opt.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int s,t;
+	printf("enter two numbers--");
+	scanf("%d %d",&s,&t);
+	printf("\n sum of the numbers is =%d",s+t);
+	printf("\n subtraction of the numbers is =%d",s-t);
+	printf("\n product of the numbers is =%d",s*t);
+	printf("\n division of the numbers is =%d",s/t);
+	printf("\n modulus of the numbers is =%d",s%t);
+	return(0);
+}
