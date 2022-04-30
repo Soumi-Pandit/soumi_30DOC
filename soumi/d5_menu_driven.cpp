f1bdc469a48dd6ac4886936ce5b5be251a8026e8
Soumi-Pandit <94689716+Soumi-Pandit@users.noreply.github.com>
2022-04-30 17:04:40 +0530

    Create d5_menu_driven.cpp
    menu driven program for basic math operations

---
+++ soumi/d5_menu_driven.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a,b,sum,sub,mul,mod,choice;
+	printf("....menu driven program for math sproblem....\n");
+	printf("1. addition\n");
+	printf("2. subtraction\n");
+	printf("3. multiplication\n");
+	printf("4. division\n");
+	printf("5. exit\n");
+	
+	
+	while(true)
+	{
+		printf("what would you you like to perform?  :");
+		scanf("%d",&choice);
+	
+		switch(choice)
+		{
+			case 1:
+				printf("enter numbers :");
+				scanf("%d %d",&a,&b);
+				printf("sum is %d \n",a+b);
+				break;
+				
+				
+			case 2:
+				printf("enter numbers :");
+				scanf("%d %d",&a,&b);
+				printf("sub is %d\n",a-b);
+				break;
+				
+			case 3:
+				printf("enter numbers :");
+				scanf("%d %d",&a,&b);
+				printf("product is %d\n",a*b);
+				break;
+				
+			case 4:
+				printf("enter numbers :");
+				scanf("%d %d",&a,&b);
+				printf("quotient is %d\n",a/b);
+				break;
+				
+			case 5:
+				exit(0);
+				break;
+		}
+			
+	}
+
+}
